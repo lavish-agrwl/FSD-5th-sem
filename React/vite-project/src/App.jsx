f78f866e9c3a28cc1f2fbc2e713a1aaf2d40fd1c
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-10-29 09:54:01 +0530

    2025-10-29 commit #21

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from "./Components/Card"
-
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Card />
-
+      <br />
+      <Card />
+      <br />
+      <Card />
 
     </div>
   )

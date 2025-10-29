7877efa7833d390be28a813e08d5fb4349eb48c7
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-10-29 09:25:29 +0530

    2025-10-29 commit #7

--- React/vite-project/src/Components/Card.jsx
+++ React/vite-project/src/Components/Card.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import "./card.css"
 
 function Card() {
   return (
-    <div>
-      <img
-        src="https://animalfactguide.com/wp-content/uploads/2025/03/panda-climbing.jpg"
-        alt=""
-        height={500}
-        width={500}
-      />
-      <h1>Lavish</h1>
+    <div id="adi">
+      <center>
+        <img
+          src="https://animalfactguide.com/wp-content/uploads/2025/03/panda-climbing.jpg"
+          alt=""
+          height={300}
+          width={300}
+        />
+        <h1>Lavish</h1>
+      </center>
     </div>
   );
 }

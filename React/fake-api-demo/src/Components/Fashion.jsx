e71819a614ae4de3e69809afe2a65510c4a217a0
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-11-12 09:24:26 +0530

    2025-11-12 commit #6

--- React/fake-api-demo/src/Components/Fashion.jsx
+++ React/fake-api-demo/src/Components/Fashion.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./fashion.css";
 
 function Fashion(props) {
   return (
     <div>
+      <div className="card">
         <h1>Fashion Show</h1>
         <img src={props.image} alt="" height={80} width={80} />
         <h3>{props.name}</h3>
         <h4>Price: {props.price}</h4>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Fashion
+export default Fashion;

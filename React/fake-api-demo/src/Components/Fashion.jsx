f39ae0e52ff8c161780f7a630d7f83f5c446611e
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-11-12 09:20:20 +0530

    2025-11-12 commit #4

--- React/fake-api-demo/src/Components/Fashion.jsx
+++ React/fake-api-demo/src/Components/Fashion.jsx
@@ -5,6 +5,8 @@ function Fashion(props) {
     <div>
         <h1>Fashion Show</h1>
         <img src={props.image} alt="" height={80} width={80} />
+        <h3>{props.name}</h3>
+        <h4>Price: {props.price}</h4>
     </div>
   )
 }

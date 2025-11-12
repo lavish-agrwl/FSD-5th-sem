419c9a942d554fbf4f45a6c7fcd1460c50ea47bc
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-11-12 09:32:42 +0530

    2025-11-12 commit #10

--- React/fake-api-demo/src/App.jsx
+++ React/fake-api-demo/src/App.jsx
@@ -1,12 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/category/women%27s%20clothing')
-      .then(res=>res.json())
-      .then(json=>setData(json))
+    fetch('https://fakestoreapi.com/products')
   }, [])
   return (
     <div>
